0c20f468f076352722db662d110f82ad6aae742c
gimzing <lukaperic93@gmail.com>
2022-01-17 23:35:45 +0800

    day4 wip

--- day4/main.go
+++ day4/main.go
@@ -1,43 +1,70 @@
-// package main
+package main
 
-// import (
-// 	"os"
-// 	"strings"
-// )
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
-// func checkError(err error) {
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// }
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
 
-// func parseInput() ([]int, []string) {
-// 	data, err := os.ReadFile("input.txt")
-// 	checkError(err)
+type bingoNumber struct {
+	number string
+	seen   bool
+}
 
-// 	inputStrings := strings.Split(string(data), "\n\n")
+// func parseInput() {
+func parseInput() ([]string, [][]bingoNumber) {
+	data, err := os.ReadFile("input.txt")
+	checkError(err)
+	inputStrings := strings.Split(string(data), "\n\n")
 
-// 	return inputStrings[0], inputStrings[1:]
-// }
+	var numbers []string = strings.Split(inputStrings[0], ",")
+	var boardsString []string = inputStrings[1:]
+	var boards [][]bingoNumber
 
-// func part1() {
-// 	// numbers, boards := parseInput()
+	for i := range boardsString {
+		var boardString string = boardsString[i]
+		boardString = strings.Replace(boardString, "\r ", "", -1)
+		boardString = strings.Replace(boardString, "\n ", "\n", -1)
+		boardString = strings.Replace(boardString, "  ", " ", -1)
 
-// 	// fmt.Println(numbers)
-// 	// fmt.Println(boards)
+		if string(boardString[0]) == " " {
+			boardString = strings.Replace(boardString, " ", "", 1)
+		}
 
-// 	// foo := strings.Split(input[1], "\n")
+		var boardSplit []string = strings.Split(boardString, " ")
 
-// 	// for x := range input {
-// 	// 	fmt.Println(input[x])
-// 	// }
-// }
+		fmt.Println(len(boardSplit))
+
+		// for j := range boardSplit {
+		// 	fmt.Println(boardSplit[j])
+		// }
+
+		// var actualBoard []bingoNumber
+
+		// boards = append(boards, board)
+	}
+
+	return numbers, boards
+}
+
+func part1() {
+	// numbers, boards = parseInput()
+	// fmt.Println("numbers", numbers)
+	// fmt.Println("boards", boards)
+}
 
 // func part2() {
 
 // }
 
-// func main() {
-// 	part1()
-// 	// part2()
-// }
+func main() {
+	parseInput()
+	// part1()
+	// part2()
+}
