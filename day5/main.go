9cc77f8d9007f1322d9fe50d71d3337d6e2e4953
gimzing <lukaperic93@gmail.com>
2022-02-16 19:13:31 +0800

    day5 part1 done

--- day5/main.go
+++ day5/main.go
@@ -44,54 +44,159 @@ func parseInput() []line {
 		checkError(err)
 
 		line := line{coordinate1: coordinate{x: x1, y: y1}, coordinate2: coordinate{x: x2, y: y2}}
-		// line := line{x1: x1, y1: y1, x2: x2, y2: y2}
 		lines = append(lines, line)
 	}
 
 	return lines
 }
 
-func findLineCoordinates(line line, axis string) []coordinate {
+func findStraightLineCoordinates(line line, axis string) []coordinate {
 	var coordinates []coordinate
 
 	if axis == "x" {
-
 		if line.coordinate1.y > line.coordinate2.y {
-
-			for i := line.coordinate2.y; i < line.coordinate1.y; i++ {
-				fmt.Println("i", i)
+			for i := line.coordinate2.y; i < line.coordinate1.y+1; i++ {
+				newCoordinate := coordinate{x: line.coordinate1.x, y: i}
+				coordinates = append(coordinates, newCoordinate)
 			}
-
 		} else if line.coordinate1.y < line.coordinate2.y {
-
-			for i := line.coordinate1.y; i < line.coordinate2.y; i++ {
-				fmt.Println("i", i)
+			for i := line.coordinate1.y; i < line.coordinate2.y+1; i++ {
+				newCoordinate := coordinate{x: line.coordinate1.x, y: i}
+				coordinates = append(coordinates, newCoordinate)
 			}
 		}
-
+	} else if axis == "y" {
+		if line.coordinate1.x > line.coordinate2.x {
+			for i := line.coordinate2.x; i < line.coordinate1.x+1; i++ {
+				newCoordinate := coordinate{x: i, y: line.coordinate1.y}
+				coordinates = append(coordinates, newCoordinate)
+			}
+		} else if line.coordinate1.x < line.coordinate2.x {
+			for i := line.coordinate1.x; i < line.coordinate2.x+1; i++ {
+				newCoordinate := coordinate{x: i, y: line.coordinate1.y}
+				coordinates = append(coordinates, newCoordinate)
+			}
+		}
 	}
-	// else if axis == "y" {
 
-	// }
-
+	// fmt.Println("\n", line)
+	// fmt.Println(coordinates)
 	return coordinates
 }
 
-func main() {
+func findDiagonalLineCoordinates(coordinates []coordinate, line line, axis string) []coordinate {
+	var diagonalCoordinates []coordinate
+
+	if axis == "x" {
+		if line.coordinate1.y > line.coordinate2.y {
+			for i := line.coordinate2.y; i < line.coordinate1.y+1; i++ {
+				newCoordinate := coordinate{x: line.coordinate1.x, y: i}
+				diagonalCoordinates = append(diagonalCoordinates, newCoordinate)
+			}
+		} else if line.coordinate1.y < line.coordinate2.y {
+			for i := line.coordinate1.y; i < line.coordinate2.y+1; i++ {
+				newCoordinate := coordinate{x: line.coordinate1.x, y: i}
+				diagonalCoordinates = append(diagonalCoordinates, newCoordinate)
+			}
+		}
+	} else if axis == "y" {
+		if line.coordinate1.x > line.coordinate2.x {
+			for i := line.coordinate2.x; i < line.coordinate1.x+1; i++ {
+				newCoordinate := coordinate{x: i, y: line.coordinate1.y}
+				diagonalCoordinates = append(diagonalCoordinates, newCoordinate)
+			}
+		} else if line.coordinate1.x < line.coordinate2.x {
+			for i := line.coordinate1.x; i < line.coordinate2.x+1; i++ {
+				newCoordinate := coordinate{x: i, y: line.coordinate1.y}
+				diagonalCoordinates = append(diagonalCoordinates, newCoordinate)
+			}
+		}
+	}
+
+	// fmt.Println("\n", line)
+	// fmt.Println(diagonalCoordinates)
+	return coordinates
+}
+
+func part1() {
+	fmt.Println("PART 1")
 	lines := parseInput()
-	var oceanFloor [10][10]int
-	fmt.Println(oceanFloor)
-	// floor := oceanFloor{}
-	// fmt.Println(lines)
-	// fmt.Println(floor)
+	// var oceanFloor [10][10]int
+	var oceanFloor [1000][1000]int
+	// fmt.Println(oceanFloor)
 
 	for _, line := range lines {
 		if line.coordinate1.x == line.coordinate2.x {
-			findLineCoordinates(line, "x")
+			coordinates := findStraightLineCoordinates(line, "x")
+
+			for i := range coordinates {
+				oceanFloor[coordinates[i].x][coordinates[i].y]++
+			}
 		}
 
 		if line.coordinate1.y == line.coordinate2.y {
-			findLineCoordinates(line, "y")
+			coordinates := findStraightLineCoordinates(line, "y")
+
+			for i := range coordinates {
+				oceanFloor[coordinates[i].x][coordinates[i].y]++
+			}
 		}
 	}
+
+	var count int = 0
+	for row := range oceanFloor {
+		for column := range oceanFloor {
+			if oceanFloor[row][column] > 1 {
+				count++
+			}
+		}
+	}
+
+	// fmt.Println(oceanFloor)
+	fmt.Println(count)
+	fmt.Println()
+}
+
+func part2() {
+	fmt.Println("PART 2")
+	lines := parseInput()
+	// var oceanFloor [10][10]int
+	var oceanFloor [1000][1000]int
+	// fmt.Println(oceanFloor)
+
+	for _, line := range lines {
+		if line.coordinate1.x == line.coordinate2.x {
+			coordinates := findStraightLineCoordinates(line, "x")
+
+			for i := range coordinates {
+				oceanFloor[coordinates[i].x][coordinates[i].y]++
+			}
+		}
+
+		if line.coordinate1.y == line.coordinate2.y {
+			coordinates := findStraightLineCoordinates(line, "y")
+
+			for i := range coordinates {
+				oceanFloor[coordinates[i].x][coordinates[i].y]++
+			}
+		}
+	}
+
+	var count int = 0
+	for row := range oceanFloor {
+		for column := range oceanFloor {
+			if oceanFloor[row][column] > 1 {
+				count++
+			}
+		}
+	}
+
+	// fmt.Println(oceanFloor)
+	fmt.Println(count)
+	fmt.Println()
+}
+
+func main() {
+	part1()
+	part2()
 }
