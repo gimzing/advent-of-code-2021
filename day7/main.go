1cabb03c431533c8d8fa756c5d881a7579d8a1fe
gimzing <lukaperic93@gmail.com>
2022-02-17 15:21:27 +0800

    day7 complete

--- day7/main.go
+++ day7/main.go
@@ -36,10 +36,10 @@ func part1(input []int) {
 
 	for x := range input {
 		total := 0
+
 		for y := range input {
 			total += int(math.Abs(float64(x) - float64(input[y])))
 		}
-		fmt.Println("total", total)
 
 		if lowestFuel == 0 {
 			lowestFuel = total
@@ -50,16 +50,45 @@ func part1(input []int) {
 		}
 	}
 
-	fmt.Println("lowestFuel", lowestFuel)
-
+	fmt.Println(lowestFuel)
+	fmt.Println()
 }
 
-// func part2() {
+func part2(input []int) {
 
-// }
+	var lowestFuel int
+
+	for x := range input {
+		total := 0
+
+		for y := range input {
+			steps := int(math.Abs(float64(x) - float64(input[y])))
+
+			// Triangular numbers formula
+			total += steps * (steps + 1) / 2
+
+			// Or the manual way
+			// fuelTotal := 0
+			// for i := 1; i < steps+1; i++ {
+			// 	fuelTotal += i
+			// }
+			// total += fuelTotal
+		}
+
+		if lowestFuel == 0 {
+			lowestFuel = total
+		} else {
+			if total < lowestFuel {
+				lowestFuel = total
+			}
+		}
+	}
+
+	fmt.Println(lowestFuel)
+}
 
 func main() {
 	input := parseInput("input.txt")
 	part1(input)
-	// part2()
+	part2(input)
 }
