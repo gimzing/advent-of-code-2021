3043fb6c541c387f1b74900981598e40e53e7ad4
gimzing <lukaperic93@gmail.com>
2022-02-18 14:22:27 +0800

    day8 complete

--- day8/main.go
+++ day8/main.go
@@ -13,6 +13,25 @@ func checkError(err error) {
 	}
 }
 
+//	 1111
+//	2    3
+//	2    3
+//	 4444
+//	5    6
+//	5    6
+//	 7777
+
+// 0: 6
+// 1: 2
+// 2: 5
+// 3: 5
+// 4: 4
+// 5: 5
+// 6: 6
+// 7: 3
+// 8: 7
+// 9: 6
+
 func parseInputPart1(filename string) [][]string {
 	data, err := os.ReadFile(filename)
 	checkError(err)
@@ -66,7 +85,6 @@ func getDigit(entry string, signal map[int]string) string {
 		return "8"
 	}
 
-	// TODO: WRONG HERE
 	if len(entry) == 5 {
 		// 2 3 5
 		counter := 0
@@ -143,166 +161,154 @@ func part1(input [][]string) {
 func part2(input [][]string) {
 	fmt.Println("Part 2")
 
-	signal := make(map[int]string)
-	signal[1] = ""
-	signal[2] = ""
-	signal[3] = ""
-	signal[4] = ""
-	signal[5] = ""
-	signal[6] = ""
-	signal[7] = ""
+	total := 0
 
-	//	 1111
-	//	2    3
-	//	2    3
-	//	 4444
-	//	5    6
-	//	5    6
-	//	 7777
+	for i := range input {
+		signal := make(map[int]string)
+		signal[1] = ""
+		signal[2] = ""
+		signal[3] = ""
+		signal[4] = ""
+		signal[5] = ""
+		signal[6] = ""
+		signal[7] = ""
 
-	// 0: 6
-	// 1: 2
-	// 2: 5
-	// 3: 5
-	// 4: 4
-	// 5: 5
-	// 6: 6
-	// 7: 3
-	// 8: 7
-	// 9: 6
+		// Segment 1
+		var one string
+		var seven string
+		var four string
+		var eight string
 
-	// Segment 1
-	var one string
-	var seven string
-	var four string
-	var eight string
-
-	for j := range input[0] {
-		if len(input[0][j]) == 2 {
-			one = input[0][j]
-		} else if len(input[0][j]) == 3 {
-			seven = input[0][j]
-		} else if len(input[0][j]) == 4 {
-			four = input[0][j]
-		} else if len(input[0][j]) == 7 {
-			eight = input[0][j]
-		}
-	}
-
-	remainingSegment1 := seven
-	for _, x := range one {
-		remainingSegment1 = strings.Replace(remainingSegment1, string(x), "", -1)
-	}
-	signal[1] = remainingSegment1
-
-	// Segment 6
-	// var six string
-	for j := range input[0] {
-		if len(input[0][j]) == 6 {
-			matchingSegmentCount := 0
-			for _, k := range input[0][j] {
-				if string(k) == string(one[0]) || string(k) == string(one[1]) {
-					matchingSegmentCount++
-				}
-			}
-
-			if matchingSegmentCount == 1 {
-				// six = input[0][j]
-				remainingSegment6 := one
-				for _, x := range input[0][j] {
-					remainingSegment6 = strings.Replace(remainingSegment6, string(x), "", -1)
-				}
-				signal[6] = remainingSegment6
+		for j := range input[i] {
+			if len(input[i][j]) == 2 {
+				one = input[i][j]
+			} else if len(input[i][j]) == 3 {
+				seven = input[i][j]
+			} else if len(input[i][j]) == 4 {
+				four = input[i][j]
+			} else if len(input[i][j]) == 7 {
+				eight = input[i][j]
 			}
 		}
-	}
 
-	// Segment 3
-	remainingSegment3 := one
-	remainingSegment3 = strings.Replace(remainingSegment3, signal[6], "", -1)
-	signal[3] = remainingSegment3
+		remainingSegment1 := seven
+		for _, x := range one {
+			remainingSegment1 = strings.Replace(remainingSegment1, string(x), "", -1)
+		}
+		signal[1] = remainingSegment1
 
-	// Segment 4
-	remainingFour := four
-	remainingFour = strings.Replace(remainingFour, signal[3], "", -1)
-	remainingFour = strings.Replace(remainingFour, signal[6], "", -1)
-	for j := range input[0] {
-		if len(input[0][j]) == 6 {
-			matchingSegmentCount := 0
-			for _, k := range input[0][j] {
-				if string(k) == string(remainingFour[0]) || string(k) == string(remainingFour[1]) {
-					matchingSegmentCount++
+		// Segment 6
+		for j := range input[i] {
+			if len(input[i][j]) == 6 {
+				matchingSegmentCount := 0
+				for _, k := range input[i][j] {
+					if string(k) == string(one[0]) || string(k) == string(one[1]) {
+						matchingSegmentCount++
+					}
 				}
-			}
 
-			if matchingSegmentCount == 1 {
-				remainingSegment4 := remainingFour
-				for _, x := range input[0][j] {
-					remainingSegment4 = strings.Replace(remainingSegment4, string(x), "", -1)
+				if matchingSegmentCount == 1 {
+					remainingSegment3 := one
+					for _, x := range input[i][j] {
+						remainingSegment3 = strings.Replace(remainingSegment3, string(x), "", -1)
+					}
+					signal[3] = remainingSegment3
 				}
-				signal[4] = remainingSegment4
 			}
 		}
-	}
 
-	// Segment 2
-	remainingSegment2 := four
-	remainingSegment2 = strings.Replace(remainingSegment2, signal[3], "", -1)
-	remainingSegment2 = strings.Replace(remainingSegment2, signal[4], "", -1)
-	remainingSegment2 = strings.Replace(remainingSegment2, signal[6], "", -1)
-	signal[2] = remainingSegment2
+		// Segment 3
+		remainingSegment6 := one
+		remainingSegment6 = strings.Replace(remainingSegment6, signal[3], "", -1)
+		signal[6] = remainingSegment6
 
-	// Segment 7
-	remainingEight := eight
-	remainingEight = strings.Replace(remainingEight, signal[1], "", -1)
-	remainingEight = strings.Replace(remainingEight, signal[2], "", -1)
-	remainingEight = strings.Replace(remainingEight, signal[3], "", -1)
-	remainingEight = strings.Replace(remainingEight, signal[4], "", -1)
-	remainingEight = strings.Replace(remainingEight, signal[6], "", -1)
-	for j := range input[0] {
-		if len(input[0][j]) == 6 {
-			matchingSegmentCount := 0
-			for _, k := range input[0][j] {
-				if string(k) == string(remainingEight[0]) || string(k) == string(remainingEight[1]) {
-					matchingSegmentCount++
+		// Segment 4
+		remainingFour := four
+		remainingFour = strings.Replace(remainingFour, signal[3], "", -1)
+		remainingFour = strings.Replace(remainingFour, signal[6], "", -1)
+		for j := range input[i] {
+			if len(input[i][j]) == 6 {
+				matchingSegmentCount := 0
+				for _, k := range input[i][j] {
+					if string(k) == string(remainingFour[0]) || string(k) == string(remainingFour[1]) {
+						matchingSegmentCount++
+					}
 				}
-			}
 
-			if matchingSegmentCount == 1 {
-				remainingSegment7 := remainingEight
-				for _, x := range input[0][j] {
-					remainingSegment7 = strings.Replace(remainingSegment7, string(x), "", -1)
+				if matchingSegmentCount == 1 {
+					remainingSegment4 := remainingFour
+					for _, x := range input[i][j] {
+						remainingSegment4 = strings.Replace(remainingSegment4, string(x), "", -1)
+					}
+					signal[4] = remainingSegment4
 				}
-				signal[7] = remainingSegment7
 			}
 		}
+
+		// Segment 2
+		remainingSegment2 := four
+		remainingSegment2 = strings.Replace(remainingSegment2, signal[3], "", -1)
+		remainingSegment2 = strings.Replace(remainingSegment2, signal[4], "", -1)
+		remainingSegment2 = strings.Replace(remainingSegment2, signal[6], "", -1)
+		signal[2] = remainingSegment2
+
+		// Segment 5
+		remainingEight := eight
+		remainingEight = strings.Replace(remainingEight, signal[1], "", -1)
+		remainingEight = strings.Replace(remainingEight, signal[2], "", -1)
+		remainingEight = strings.Replace(remainingEight, signal[3], "", -1)
+		remainingEight = strings.Replace(remainingEight, signal[4], "", -1)
+		remainingEight = strings.Replace(remainingEight, signal[6], "", -1)
+		for j := range input[i] {
+			if len(input[i][j]) == 6 {
+				matchingSegmentCount := 0
+				for _, k := range input[i][j] {
+					if string(k) == string(remainingEight[0]) || string(k) == string(remainingEight[1]) {
+						matchingSegmentCount++
+					}
+				}
+				if matchingSegmentCount == 1 {
+					remainingSegment5 := remainingEight
+
+					for _, x := range input[i][j] {
+
+						remainingSegment5 = strings.Replace(remainingSegment5, string(x), "", -1)
+					}
+					signal[5] = remainingSegment5
+				}
+			}
+		}
+
+		// Segment 7
+		remainingEight = eight
+		remainingEight = strings.Replace(remainingEight, signal[1], "", -1)
+		remainingEight = strings.Replace(remainingEight, signal[2], "", -1)
+		remainingEight = strings.Replace(remainingEight, signal[3], "", -1)
+		remainingEight = strings.Replace(remainingEight, signal[4], "", -1)
+		remainingEight = strings.Replace(remainingEight, signal[5], "", -1)
+		remainingEight = strings.Replace(remainingEight, signal[6], "", -1)
+		signal[7] = remainingEight
+
+		lastFour := input[i][10:]
+
+		var result string
+		for index := range lastFour {
+			result += getDigit(lastFour[index], signal)
+		}
+
+		value, err := strconv.Atoi(result)
+		checkError(err)
+
+		total += value
 	}
 
-	// Segment 5
-	remainingEight = eight
-	remainingEight = strings.Replace(remainingEight, signal[1], "", -1)
-	remainingEight = strings.Replace(remainingEight, signal[2], "", -1)
-	remainingEight = strings.Replace(remainingEight, signal[3], "", -1)
-	remainingEight = strings.Replace(remainingEight, signal[4], "", -1)
-	remainingEight = strings.Replace(remainingEight, signal[6], "", -1)
-	remainingEight = strings.Replace(remainingEight, signal[7], "", -1)
-	signal[5] = remainingEight
-
-	lastFour := input[0][len(input[0])-4:]
-
-	var result string
-	for i := range lastFour {
-		result += getDigit(lastFour[i], signal)
-	}
-	value, err := strconv.Atoi(result)
-	checkError(err)
-
-	fmt.Println(value, "8394")
+	fmt.Println(total)
+	fmt.Println()
 }
 
 func main() {
-	input1 := parseInputPart1("test.txt")
-	input2 := parseInputPart2("test.txt")
+	input1 := parseInputPart1("input.txt")
+	input2 := parseInputPart2("input.txt")
 
 	part1(input1)
 	part2(input2)
